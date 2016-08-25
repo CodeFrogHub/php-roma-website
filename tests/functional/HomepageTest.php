88de64a1adfe4bab8db9992c9c07863aa66b4876
Steffen Bahrmann <steffen.bahrmann@codefrog-it.de>
2016-08-25 09:04:59 +0200

    test for travis-ci

--- tests/functional/HomepageTest.php
+++ tests/functional/HomepageTest.php
@@ -32,7 +32,7 @@ class HomepageTest extends BaseTestCase
     public function testPostHomepageNotAllowed()
     {
         $response = $this->runApp('POST', '/', ['test']);
-
+ 
         $this->assertEquals(405, $response->getStatusCode());
         $this->assertContains('Method not allowed', (string)$response->getBody());
     }
